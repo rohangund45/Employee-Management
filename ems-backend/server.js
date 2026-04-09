32a9dac1e345ff1d92f6ce687a9a8e796797730b
rohangund45 <rohangund45@gmail.com>
2026-04-10 01:00:17 +0530

    Fix CORS for production

--- ems-backend/server.js
+++ ems-backend/server.js
@@ -16,7 +16,13 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 const app = express();
 
 // --- Middleware ---
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://employee-management-sys-seven.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
